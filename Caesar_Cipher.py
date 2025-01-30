ae1981c25c0f31b6f25cf57520eb04bc110308a5
Ayush Gond <ayushgond1112@gmail.com>
2025-01-30 22:04:19 +0530

    Add files via upload

---
+++ Caesar_Cipher.py
@@ -0,0 +1,39 @@
+#Caesar Cipher
+a = input("Type 'Encrypt' for encryption, type 'decrypt' for decryption: ")
+
+b = input("Type your message: ").upper()
+
+c =int(input("Type the shift number: "))
+
+list1=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','y','Y','Z']
+#while n!=0:
+if a=='Encrypt':
+    for i in b:
+        if i in list1:
+            d = (list1.index(i))
+            e = (d + c)%26
+            f = list1[e]
+            g = f.lower()
+            print(g,end="")
+    
+elif a=='decrypt':
+    for j in b:
+        if j in list1:
+            h = (list1.index(j))
+            l = (h - c)
+            if l<0:
+                l = l+26
+                x = l%26
+                m = list1[x]
+                n = m.lower()
+                print(n,end="")
+            else:
+                l = (h - c)%26
+                m = list1[l]
+                n = m.lower()
+                print(n,end="")
+
+
+
+
+                
